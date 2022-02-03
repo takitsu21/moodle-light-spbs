2427204fe742dfc2c8df80d1a6034680583a399f
margauxschmied <margaux.schmied@icloud.com>
2022-02-03 16:54:15 +0100

    refactor Ressource

--- src/main/java/Ressource/Ressource.java
+++ src/main/java/Ressource/Ressource.java
@@ -1,8 +1,15 @@
 package Ressource;
 
+import Ressource.Cours.Cours;
+import Ressource.Exercices.Questionnaire;
+
+import java.util.ArrayList;
+
 public class Ressource {
     private String name;
     private String description;
+    private ArrayList<Cours> cours;
+    private ArrayList<Questionnaire> questionnaires;
 
     public Ressource(String name, String description) {
         this.name = name;
