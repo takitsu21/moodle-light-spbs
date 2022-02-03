9ea690a439679e90a5dbdc86f8a63b17bf3ed266
Dylann B <dylannbatisse@gmail.com>
2022-02-03 16:55:26 +0100

    use List

--- src/main/java/Ressource/Ressource.java
+++ src/main/java/Ressource/Ressource.java
@@ -3,13 +3,13 @@ package Ressource;
 import Ressource.Cours.Cours;
 import Ressource.Exercices.Questionnaire;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class Ressource {
     private String name;
     private String description;
-    private ArrayList<Cours> cours;
-    private ArrayList<Questionnaire> questionnaires;
+    private List<Cours> cours;
+    private List<Questionnaire> questionnaires;
 
     public Ressource(String name, String description) {
         this.name = name;
